0aa7d654f6f84a4eed6322694cb60a84c837fb51
Henry Borchers <henryborchers@yahoo.com>
2024-09-04 14:40:27 -0500

    0.2.0

--- uiucprescon/build/__init__.py
+++ uiucprescon/build/__init__.py
@@ -1,7 +1,7 @@
 from .local_backend import build_sdist, \
     build_wheel, prepare_metadata_for_build_wheel, get_requires_for_build_sdist
 
-VERSION = "0.1.3.dev0"
+VERSION = "0.2.0"
 
 __all__ = [
     'build_sdist',
