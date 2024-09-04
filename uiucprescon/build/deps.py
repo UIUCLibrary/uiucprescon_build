addb6e23dd83473ec7e5cf5afdf7056e966d4008
Henry Borchers <henryborchers@yahoo.com>
2024-09-04 13:53:16 -0500

    improve ways of finding dumpbin when building for windows

--- uiucprescon/build/deps.py
+++ uiucprescon/build/deps.py
@@ -1,7 +1,12 @@
+import contextlib
+import os
 import re
+import subprocess
 import sys
-from typing import List
+from distutils.util import get_platform
+from typing import List, Callable, Optional
 from distutils.ccompiler import CCompiler
+from .msvc import msvc14_get_vc_env
 import shutil
 DEPS_REGEX = \
     r'(?<=(Image has the following dependencies:(\n){2}))((?<=\s).*\.dll\n)*'
@@ -39,15 +44,87 @@ def remove_system_dlls(dlls: List[str]) -> List[str]:
     return non_system_dlls
 
 
+def locate_dumpbin_via_path() -> Optional[str]:
+    vc_env = msvc14_get_vc_env(get_platform())
+    for path in vc_env.get('path', '').split(";"):
+        dumpbin_exe = shutil.which('dumpbin', path=path)
+        if dumpbin_exe is not None:
+            return dumpbin_exe
+    return None
+
+
+def locate_dumpbin_using_vs_where() -> Optional[str]:
+    variant = 'arm64' if get_platform() == 'win-arm64' else 'x86.x64'
+    suitable_components = (
+        f"Microsoft.VisualStudio.Component.VC.Tools.{variant}",
+        "Microsoft.VisualStudio.Workload.WDExpress",
+    )
+
+    for component in suitable_components:
+        root = (
+            os.environ.get("ProgramFiles(x86)") or
+            os.environ.get("ProgramFiles")
+        )
+        if not root:
+            return None
+        expected_path_values = {
+            'win-amd64': "Hostx64\\x64"
+        }
+        with contextlib.suppress(
+                subprocess.CalledProcessError, OSError, UnicodeDecodeError
+        ):
+            dumpbin_locations = (
+                subprocess.check_output([
+                    os.path.join(
+                        root, "Microsoft Visual Studio",
+                        "Installer", "vswhere.exe"
+                    ),
+                    "-latest",
+                    "-prerelease",
+                    "-requires",
+                    component,
+                    "-find",
+                    "**/dumpbin.exe",
+                ])
+                .decode(encoding="mbcs", errors="strict")
+                .strip().split("\r\n")
+            )
+            _platform = get_platform()
+            for location in dumpbin_locations:
+                expected_path_value = expected_path_values.get(_platform)
+                if expected_path_value is None:
+                    raise ValueError(f"Unsupported platform: {_platform}")
+                if expected_path_value in location:
+                    return location
+            return None
+
+
+FIND_DUMPBIN_STRATEGIES_DEFAULT_ORDER: List[Callable[[], Optional[str]]] = [
+    locate_dumpbin_via_path,
+    locate_dumpbin_using_vs_where
+]
+
+
 def get_win_deps(
         dll_name: str,
         output_file: str,
         compiler: CCompiler
 ) -> List[str]:
-    dumpbin_exe = shutil.which('dumpbin')
-    if dumpbin_exe is None:
+
+    if not compiler.initialized:
+        compiler.initialize()
+
+    for strategy in FIND_DUMPBIN_STRATEGIES_DEFAULT_ORDER:
+        dumpbin_exe = strategy()
+        if dumpbin_exe is not None:
+            break
+    else:
         dumpbin_exe = 'dumpbin'
-        print('Unable to locate dumpbin', file=sys.stderr)
+        print(
+            'Unable to locate dumpbin. Guessing it will be on that path when '
+            'it is needed',
+            file=sys.stderr
+        )
 
     compiler.spawn(
         [
