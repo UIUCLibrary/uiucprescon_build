76b345e4772eb8ea638d80164898d9641fa501d2
Henry Borchers <henryborchers@yahoo.com>
2025-06-23 15:30:39 -0500

    fix up shared libraries

--- src/uiucprescon/build/deps.py
+++ src/uiucprescon/build/deps.py
@@ -1,32 +1,44 @@
 import contextlib
 import os
+import platform
 import re
 import subprocess
 import sys
+import warnings
+
 from distutils.util import get_platform
-from typing import List, Callable, Optional
+from typing import List, Callable, Optional, Union, Set, Dict, cast
 from distutils.ccompiler import CCompiler
 from .msvc import msvc14_get_vc_env
 import shutil
-DEPS_REGEX = \
-    r'(?<=(Image has the following dependencies:(\n){2}))((?<=\s).*\.dll\n)*'
+
+DEPS_REGEX = (
+    r"(?<=(Image has the following dependencies:(\n){2}))((?<=\s).*\.dll\n)*"
+)
+
+
+def parse_dumpbin_data(data: str) -> List[str]:
+    dlls = []
+    dep_regex = re.compile(DEPS_REGEX)
+    print(f"Using {DEPS_REGEX}")
+    d = dep_regex.search(data)
+    if d is None:
+        print(
+            "Data from dumpbin does not match expected format", file=sys.stderr
+        )
+        print(data, file=sys.stderr)
+        raise ValueError("unable to parse dumpbin file")
+    for x in d.group(0).split("\n"):
+        if x.strip() == "":
+            continue
+        dll = x.strip()
+        dlls.append(dll)
+    return dlls
 
 
 def parse_dumpbin_deps(file: str) -> List[str]:
-
-    dlls = []
-    dep_regex = re.compile(DEPS_REGEX)
-
     with open(file) as f:
-        d = dep_regex.search(f.read())
-        if d is None:
-            raise ValueError('unable to parse dumpbin file')
-        for x in d.group(0).split("\n"):
-            if x.strip() == "":
-                continue
-            dll = x.strip()
-            dlls.append(dll)
-    return dlls
+        return parse_dumpbin_data(f.read())
 
 
 def remove_system_dlls(dlls: List[str]) -> List[str]:
@@ -46,48 +58,50 @@ def remove_system_dlls(dlls: List[str]) -> List[str]:
 
 def locate_dumpbin_via_path() -> Optional[str]:
     vc_env = msvc14_get_vc_env(get_platform())
-    for path in vc_env.get('path', '').split(";"):
-        dumpbin_exe = shutil.which('dumpbin', path=path)
+    for path in vc_env.get("path", "").split(";"):
+        dumpbin_exe = shutil.which("dumpbin", path=path)
         if dumpbin_exe is not None:
             return dumpbin_exe
     return None
 
 
 def locate_dumpbin_using_vs_where() -> Optional[str]:
-    variant = 'arm64' if get_platform() == 'win-arm64' else 'x86.x64'
+    variant = "arm64" if get_platform() == "win-arm64" else "x86.x64"
     suitable_components = (
         f"Microsoft.VisualStudio.Component.VC.Tools.{variant}",
         "Microsoft.VisualStudio.Workload.WDExpress",
     )
 
     for component in suitable_components:
-        root = (
-            os.environ.get("ProgramFiles(x86)") or
-            os.environ.get("ProgramFiles")
+        root = os.environ.get("ProgramFiles(x86)") or os.environ.get(
+            "ProgramFiles"
         )
         if not root:
             return None
-        expected_path_values = {
-            'win-amd64': "Hostx64\\x64"
-        }
+        expected_path_values = {"win-amd64": "Hostx64\\x64"}
         with contextlib.suppress(
-                subprocess.CalledProcessError, OSError, UnicodeDecodeError
+            subprocess.CalledProcessError, OSError, UnicodeDecodeError
         ):
             dumpbin_locations = (
-                subprocess.check_output([
-                    os.path.join(
-                        root, "Microsoft Visual Studio",
-                        "Installer", "vswhere.exe"
-                    ),
-                    "-latest",
-                    "-prerelease",
-                    "-requires",
-                    component,
-                    "-find",
-                    "**/dumpbin.exe",
-                ])
+                subprocess.check_output(
+                    [
+                        os.path.join(
+                            root,
+                            "Microsoft Visual Studio",
+                            "Installer",
+                            "vswhere.exe",
+                        ),
+                        "-latest",
+                        "-prerelease",
+                        "-requires",
+                        component,
+                        "-find",
+                        "**/dumpbin.exe",
+                    ]
+                )
                 .decode(encoding="mbcs", errors="strict")
-                .strip().split("\r\n")
+                .strip()
+                .split("\r\n")
             )
             _platform = get_platform()
             for location in dumpbin_locations:
@@ -101,16 +115,19 @@ def locate_dumpbin_using_vs_where() -> Optional[str]:
 
 FIND_DUMPBIN_STRATEGIES_DEFAULT_ORDER: List[Callable[[], Optional[str]]] = [
     locate_dumpbin_via_path,
-    locate_dumpbin_using_vs_where
+    locate_dumpbin_using_vs_where,
 ]
 
 
 def get_win_deps(
-        dll_name: str,
-        output_file: str,
-        compiler: CCompiler
+    dll_name: str, output_file: str, compiler: CCompiler
 ) -> List[str]:
-
+    warnings.warn(
+        "get_win_deps is deprecated, use "
+        "use_dumpbin_to_determine_deps instead",
+        category=DeprecationWarning,
+        stacklevel=2,
+    )
     if not compiler.initialized:
         compiler.initialize()
 
@@ -119,21 +136,250 @@ def get_win_deps(
         if dumpbin_exe is not None:
             break
     else:
-        dumpbin_exe = 'dumpbin'
+        dumpbin_exe = "dumpbin"
         print(
-            'Unable to locate dumpbin. Guessing it will be on that path when '
-            'it is needed',
-            file=sys.stderr
+            "Unable to locate dumpbin. Guessing it will be on that path when "
+            "it is needed",
+            file=sys.stderr,
         )
 
     compiler.spawn(
-        [
-            dumpbin_exe,
-            '/dependents',
-            dll_name,
-            f'/out:{output_file}'
-        ]
+        [dumpbin_exe, "/dependents", dll_name, f"/out:{output_file}"]
     )
     deps = parse_dumpbin_deps(file=output_file)
     deps = remove_system_dlls(deps)
     return deps
+
+
+def use_dumpbin_to_determine_deps(library_path: str) -> List[str]:
+    from setuptools.msvc import EnvironmentInfo
+
+    visual_studio_info = EnvironmentInfo("amd64")
+    for path in visual_studio_info.return_env()["path"].split(";"):
+        if not os.path.exists(path):
+            continue
+        dumpbin = shutil.which("dumpbin.exe", path=path)
+        if dumpbin is not None:
+            break
+
+    if dumpbin is None:
+        raise FileNotFoundError("Unable to locate dumpbin.exe")
+
+    dumpbin_command = [dumpbin, "/nologo", "/dependents", library_path]
+    process = subprocess.run(
+        dumpbin_command,
+        env={**os.environ.copy(), **visual_studio_info.return_env()},
+        check=True,
+        capture_output=True,
+        encoding="mbcs",
+        errors="strict",
+    )
+    return parse_dumpbin_data(process.stdout)
+
+
+def use_readelf_to_determine_deps(
+    library_path: str,
+    run_readelf_strategy: Optional[Callable[[str], str]] = None,
+) -> List[str]:
+    def _run_readelf(_library_path: str) -> str:
+        readelf = shutil.which("readelf")
+        if readelf is None:
+            raise FileNotFoundError("readelf not found")
+        return subprocess.run(
+            [readelf, "-d", _library_path],
+            check=True,
+            capture_output=True,
+            encoding="utf8",
+        ).stdout
+
+    run_readelf: Callable[[str], str] = run_readelf_strategy or _run_readelf
+    deps = []
+    system_libs = ["libm", "libstdc++", "libgcc", "libc", "libpthread"]
+    for line in run_readelf(library_path).split("\n"):
+        if "Shared library:" in line:
+            parts = line.split()
+            lib = parts[-1].lstrip("[").rstrip("]")
+            is_system_lib = False
+            for system_lib in system_libs:
+                if system_lib in lib:
+                    is_system_lib = True
+                    continue
+            if not is_system_lib:
+                deps.append(lib)
+    return deps
+
+
+def fix_up_linux_libraries(
+    library: str,
+    search_paths: List[str],
+    exclude_libraries: Optional[Union[Set[str], List[str]]] = None,
+) -> None:
+    output_path = os.path.dirname(library)
+    patchelf = shutil.which("patchelf")
+    if patchelf is None:
+        raise FileNotFoundError("patchelf not found")
+    for library in use_readelf_to_determine_deps(library):
+        for path in search_paths:
+            matching_library = os.path.join(path, library)
+            if not os.path.exists(matching_library):
+                continue
+            copied_library = os.path.join(output_path, library)
+            if not os.path.exists(copied_library):
+                print(f"Copying {matching_library} to {copied_library}")
+                shutil.copy2(matching_library, output_path)
+                subprocess.run(
+                    [patchelf, "--set-rpath", "$ORIGIN", copied_library],
+                    check=True,
+                )
+                fix_up_linux_libraries(
+                    copied_library, search_paths, exclude_libraries
+                )
+            break
+        else:
+            raise FileNotFoundError(
+                f"Unable to locate {library} in search paths"
+            )
+
+
+def fix_up_darwin_libraries(
+    library: str,
+    search_paths: List[str],
+    exclude_libraries: Optional[Union[Set[str], List[str]]] = None,
+) -> None:
+    otool = shutil.which("otool")
+    install_name_tool = shutil.which("install_name_tool")
+    if not all([otool, install_name_tool]):
+        raise FileNotFoundError(
+            "Unable to fixed up because required tools are missing. "
+            "Make sure that otool and install_name_tool are on "
+            "the PATH."
+        )
+
+    # Hack: Casting the following to strings to help MyPy which doesn't
+    #  currently (as of mypy version 1.6.0) understand that if they any
+    #  were None, they'd raise a FileNotFound error.
+    otool = cast(str, otool)
+    install_name_tool = cast(str, install_name_tool)
+
+    dylib_regex = re.compile(
+        r"^(?P<path>([@a-zA-Z./_])+)"
+        r"/"
+        r"(?P<file>lib[a-zA-Z/.0-9]+\.dylib)"
+    )
+    for line in subprocess.check_output(
+        [otool, "-L", library], encoding="utf8"
+    ).split("\n"):
+        if any(
+            [
+                line.strip() == "",  # it's an empty line
+                str(library) in line,  # it's the same library
+                "/usr/lib/" in line,  # it's a system library
+                "/System/Library/Frameworks/" in line,  # it's a system library
+            ]
+        ):
+            continue
+        value = dylib_regex.match(line.strip())
+        if value:
+            try:
+                original_path = value.group("path")
+                depending_library_name = value["file"].strip()
+            except AttributeError as e:
+                raise ValueError(f"unable to parse {line}") from e
+        else:
+            raise ValueError(f"unable to parse {line}")
+        if exclude_libraries:
+            if depending_library_name.lower() in {
+                lib.lower() for lib in exclude_libraries
+            }:
+                continue
+        output_path = os.path.dirname(library)
+        if not os.path.exists(
+            os.path.join(output_path, depending_library_name)
+        ):
+            for path in search_paths:
+                matching_library = os.path.join(path, depending_library_name)
+                print(f"searching in {path}")
+                if not os.path.exists(matching_library):
+                    continue
+                shutil.copy2(matching_library, output_path)
+                fix_up_darwin_libraries(
+                    os.path.join(output_path, depending_library_name),
+                    search_paths,
+                    exclude_libraries,
+                )
+                break
+            else:
+                raise FileNotFoundError(
+                    "unable to find matching library: "
+                    f"{depending_library_name}"
+                )
+
+        command = [
+            install_name_tool,
+            "-change",
+            os.path.join(original_path, depending_library_name),
+            os.path.join("@loader_path", depending_library_name),
+            str(library),
+        ]
+        subprocess.check_call(command)
+
+
+def fix_up_windows_libraries(
+    library: str,
+    search_paths: List[str],
+    exclude_libraries: Optional[Union[Set[str], List[str]]] = None,
+    determine_dependencies_strategy: Callable[
+        [str], List[str]
+    ] = use_dumpbin_to_determine_deps,
+) -> None:
+    depending_libraries = remove_system_dlls(
+        determine_dependencies_strategy(library)
+    )
+    output_path = os.path.dirname(library)
+    for depending_library in depending_libraries:
+        if exclude_libraries:
+            if depending_library.lower() in {
+                lib.lower() for lib in exclude_libraries
+            }:
+                continue
+        if not os.path.exists(os.path.join(output_path, depending_library)):
+            for path in search_paths:
+                matching_dll = os.path.join(
+                    path, os.path.basename(depending_library)
+                )
+                if os.path.exists(matching_dll):
+                    output_library = os.path.join(
+                        output_path, depending_library
+                    )
+                    print(f"Copying {matching_dll} to {output_library}")
+                    shutil.copy2(matching_dll, output_library)
+                    fixup_library(
+                        output_library, search_paths, exclude_libraries
+                    )
+                    break
+            else:
+                raise FileNotFoundError(
+                    f"Unable to locate {depending_library}"
+                )
+
+
+DEFAULT_FIXUP_LIBRARY_STRATEGIES: Dict[
+    str, Callable[[str, List[str], Optional[Union[Set[str], List[str]]]], None]
+] = {
+    "Windows": fix_up_windows_libraries,
+    "Darwin": fix_up_darwin_libraries,
+    "Linux": fix_up_linux_libraries,
+}
+
+
+def fixup_library(
+    library: str,
+    search_paths: List[str],
+    exclude_libraries: Optional[Union[Set[str], List[str]]] = None,
+):
+    fix_up_strategy = DEFAULT_FIXUP_LIBRARY_STRATEGIES.get(platform.system())
+    if fix_up_strategy is None:
+        raise NotImplementedError(
+            f"Fixup strategy for {platform.system()} is not implemented"
+        )
+    fix_up_strategy(library, search_paths, exclude_libraries)
