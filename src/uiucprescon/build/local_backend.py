f4580e748a6ec7fded9877b2373674f4d3e0316b
Henry Borchers <henryborchers@yahoo.com>
2025-07-22 12:03:00 -0500

    fix issue with os.environ becoming case sensitive on case-insensitive os

--- src/uiucprescon/build/local_backend.py
+++ src/uiucprescon/build/local_backend.py
@@ -24,12 +24,26 @@ def build_sdist(
 @contextlib.contextmanager
 def set_env_var(env_vars: Dict[str, str]):
     """Set an environment variable."""
-    og = os.environ.copy()
-    try:
-        os.environ.update(env_vars)
-        yield
-    finally:
-        os.environ = og
+    og_env = os.environ.copy()
+    os.environ.update(**env_vars)
+
+    yield
+
+    # Do not assign os.environ to og_env because it makes os.environ
+    # case-sensitive, even on OSs that have case-insensitive environment
+    # variables such as windows. When this happens it makes it hard to use
+    # os.environ and breaks setuptools trying to find Visual Studio.
+
+    # Remove any keys that were set since entering the context
+    for k in [
+        k for k in os.environ.keys()
+        if k not in og_env
+    ]:
+        del os.environ[k]
+
+    # set the values of environment variables before entering the context
+    for k, v in og_env.items():
+        os.environ[k] = v
 
 
 def build_wheel(
